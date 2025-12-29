48cf27bb4a522ddb434a4d9df9be218a80819bb3
Khusen <s44q.khusen@gmail.com>
2025-12-29 15:58:05 +0700

    Fix publicPath for GitHub Pages

--- webpack.common.js
+++ webpack.common.js
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/StoryApp44/",
     clean: true,
     assetModuleFilename: "images/[name][ext]",
   },
